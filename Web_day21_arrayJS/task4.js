8259d5ac03c36df54f2782305285fb73bb7aef07
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-01-28 19:46:57 +0600

    task 4 completed

---
+++ Web_day21_arrayJS/task4.js
@@ -0,0 +1,12 @@
+let a = 1;
+let b = 2;
+let c ="[]";
+let d = [];
+let e = [1,2,3];
+
+if(Array.isArray(d)){
+    console.log("The variable is an array");
+}
+else{
+    console.log("The variable is not an array");
+}
