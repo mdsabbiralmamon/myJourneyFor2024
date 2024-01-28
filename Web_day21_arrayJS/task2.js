873bf65d6e1ebca341fbbc19dc6f0fc405dc35c5
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-01-28 18:57:28 +0600

    task 2 completed

---
+++ Web_day21_arrayJS/task2.js
@@ -0,0 +1,5 @@
+var touristsSpots = ["Coxs Bazzar", "Bandarban", "chittagong"];
+touristsSpots.push("National Zoo");
+touristsSpots.push("Novo Theater", "Safari Park");
+touristsSpots.pop("Safari Park");
+console.log(touristsSpots);
