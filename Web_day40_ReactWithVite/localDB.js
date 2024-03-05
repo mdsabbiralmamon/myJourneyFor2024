bbf2b231ef44a04eb06aa7ab0e8711937ab34aec
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-03-06 00:42:20 +0600

    added product conatainer and js functions

--- Web_day40_ReactWithVite/localDB.js
+++ Web_day40_ReactWithVite/localDB.js
@@ -1,1 +1,14 @@
-console.log('connected');
+//Targets
+
+const productName = document.getElementById('productName');
+const productQuantity = document.getElementById('productQuantity');
+
+//Button click
+
+const addProduct = () => {
+    const getProduct = productName.value;
+    const getQuantity = productQuantity.value;
+    console.log(getProduct, getQuantity);
+    productName.value = '';
+    productQuantity.value = '';
+}
