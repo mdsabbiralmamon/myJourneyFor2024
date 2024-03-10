0552468dc4bd98f8cfbe6d85f62e3ec73fcb90e7
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-03-10 23:27:02 +0600

    created countries components

--- Web_day43_ReactDay3/world-tour/src/App.jsx
+++ Web_day43_ReactDay3/world-tour/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Countries from './components/Countries/Countries'
 
 function App() {
 
   return (
     <>
       <h1>React World Tour</h1>
+      <Countries></Countries>
     </>
   )
 }
---
+++ Web_day43_ReactDay3/world-tour/src/components/Countries/Countries.jsx
@@ -0,0 +1,9 @@
+const Countries = () => {
+    return (
+        <div>
+            <h3>Countries</h3>
+        </div>
+    );
+};
+
+export default Countries;
