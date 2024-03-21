688f6a2fc75f2540af9289d051fb201318e6bb86
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-03-21 19:20:37 +0600

    modified : added form comp

---
+++ Web_day47_DataFields/form-master/src/components/SimpleForm/SimpleForm.jsx
@@ -0,0 +1,13 @@
+const SimpleForm = () => {
+    return (
+        <div>
+            <form>
+                <input type="text" />
+                <br />
+                <button>Submit</button>
+            </form>
+        </div>
+    );
+};
+
+export default SimpleForm;
