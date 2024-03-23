78b8b704b9d0f843c979bc903647ccf72baca5de
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-03-23 15:30:14 +0600

    modified : error page design

--- Web_day48_CareerHub/career-hub/src/components/ErrorPage/ErrorPage.jsx
+++ Web_day48_CareerHub/career-hub/src/components/ErrorPage/ErrorPage.jsx
@@ -1,12 +1,11 @@
+import { Link } from 'react-router-dom';
 import './ErrorPage.css'
 
 const ErrorPage = () => {
     return (
         <section className="page_404">
             <div className="container mx-auto">
-                <div className="row">
-                    <div className="col-sm-12 ">
-                        <div className="col-sm-10 col-sm-offset-1  text-center">
+                        <div className="text-center">
                             <div className="four_zero_four_bg">
                                 <h1 className="text-center ">404</h1>
 
@@ -20,12 +19,10 @@ const ErrorPage = () => {
 
                                 <p>the page you are looking for not avaible!</p>
 
-                                <a href="/" className="link_404">Go to Home</a>
+                                <Link to = "/" className='btn btn-neutral mt-6'>Home</Link>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
         </section>
     );
 };
